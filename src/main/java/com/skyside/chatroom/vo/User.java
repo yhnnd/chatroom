d9319792c18cde9a59853bc6ba9a212af1dbf1c0
yhn <yhn1210460391@icloud.com>
2019-04-12 10:27:53 +0800

    update all files without minor changes

---
+++ src/main/java/com/skyside/chatroom/vo/User.java
@@ -0,0 +1,67 @@
+package com.skyside.chatroom.vo;
+
+public class User {
+    private int userid;
+    private String username;
+    private String password;
+    private String gender;
+    private String avatar;
+    private String role;
+
+    public int getUserid() {
+        return userid;
+    }
+
+    public void setUserid(int userid) {
+        this.userid = userid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public User(int userid, String username, String password, String gender, String avatar, String role) {
+        this.userid = userid;
+        this.username = username;
+        this.password = password;
+        this.gender = gender;
+        this.avatar = avatar;
+        this.role = role;
+    }
+}
