26d78b7791b9eb106a0a080d49d265c2c1e99c57
yhnnd <yhn1210460391@icloud.com>
2019-05-09 19:33:47 +0800

    Update UserCreateRoomServlet.java
    Modify InsertUserAndRoom function. Providing more parameters to avoid wrong status and fixing the bug that room founder need to approve his joining room request which is irrational.

--- src/main/java/com/skyside/chatroom/servlet/UserCreateRoomServlet.java
+++ src/main/java/com/skyside/chatroom/servlet/UserCreateRoomServlet.java
@@ -6,6 +6,7 @@ import com.skyside.chatroom.dao.RoomDAO;
 import com.skyside.chatroom.dao.UserAndRoomDAO;
 import com.skyside.chatroom.service.RoomManagerService;
 import com.skyside.chatroom.vo.Room;
+import com.skyside.chatroom.vo.UserAndRoom;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -16,11 +17,12 @@ import javax.servlet.annotation.WebServlet;
 @WebServlet("/create-room")
 public class UserCreateRoomServlet extends HttpServlet {
     private Gson gson = new Gson();
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) {
         try {
             doPost(request, response);
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -37,35 +39,35 @@ public class UserCreateRoomServlet extends HttpServlet {
             int live = Integer.parseInt(request.getParameter("room-live"));
             // 创建房间
             RoomDAO roomDAO = new RoomDAO();
-            Room room = roomDAO.InsertRoom(roomName,roomType,live==1?true:false);
+            Room room = roomDAO.InsertRoom(roomName, roomType, live == 1 ? true : false);
             // 初始化系统返回消息
             JsonObject message = new JsonObject();
             message.addProperty("user-id", 0);
             message.addProperty("username", "system");
             message.addProperty("room-id", 0);
             // 如果房间创建成功
-            if(room!=null){
+            if (room != null) {
                 // 建立用户房间联系
                 UserAndRoomDAO userAndRoomDAO = new UserAndRoomDAO();
-                int result = userAndRoomDAO.InsertUserAndRoomByUseridAndRoomid(userid,room.getRoomid());
-                if(result == 1) {
+                UserAndRoom userAndRoom = userAndRoomDAO.InsertUserAndRoom(userid, room.getRoomid(), 4, 1, 1, null);
+                if (userAndRoom != null) {
                     // 用户房间联系建立成功
                     // 设置用户为房主
-                    RoomManagerService.InsertRoomFounder(room.getRoomid(),userid);
+                    RoomManagerService.InsertRoomFounder(room.getRoomid(), userid);
                     message.addProperty("badge-class", "success");
                     message.addProperty("text-class", "success");
                     message.addProperty("message", "create room success");
-                }else{
+                } else {
                     // 用户房间联系建立失败
-                    message.addProperty("badge-class","danger");
-                    message.addProperty("text-class","danger");
-                    message.addProperty("message","create room succeeded. but join room failed.");
+                    message.addProperty("badge-class", "danger");
+                    message.addProperty("text-class", "danger");
+                    message.addProperty("message", "create room succeeded. but join room failed.");
                 }
-            }else{
+            } else {
                 // 房间创建失败
-                message.addProperty("badge-class","danger");
-                message.addProperty("text-class","danger");
-                message.addProperty("message","create room failed. room cannot be created.");
+                message.addProperty("badge-class", "danger");
+                message.addProperty("text-class", "danger");
+                message.addProperty("message", "create room failed. room cannot be created.");
             }
             // 输出消息
             out.println(gson.toJson(message));
